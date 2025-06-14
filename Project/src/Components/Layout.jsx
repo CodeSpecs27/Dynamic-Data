cd500fb065c94b4216ca7991413e193514420fda
Piyush Kumar <piyush.2710.kumar@gmail.com>
2025-06-14 18:55:52 +0530

    Setup Done

---
+++ Project/src/Components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export const Layout = () => {
+  
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 sm:px-6 lg:px-8">
+        <div className="bg-white p-6 sm:p-8 rounded-xl shadow-md w-full max-w-md">
+           <h1 className = "flex  justify-center">Billionare</h1>
+        </div>
+      </div>
+    </>
+  );
+};
