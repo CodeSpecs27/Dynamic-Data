c2ba93141c9516844e26f80f08a42f1b1c0472c7
Piyush Kumar <piyush.2710.kumar@gmail.com>
2025-06-16 01:11:58 +0530

    Updated StudentDetails Component

--- Project/src/Components/StudentSelector.jsx
+++ Project/src/Components/StudentSelector.jsx
@@ -4,7 +4,7 @@ export const StudentSelector = ({students, selectedId, setSelectedId}) => {
   return (
     <div className = "w-full md:w-1/3">
         <label htmlFor='studentSelect' className = "lock mb-2 font-semibold">
-            Select a Student :
+            Select a Billionare :
         </label>
         <select
            id = "studentSelect" className = "w-full p-2 border rounded" value = {selectedId} onChange = { e => setSelectedId(e.target.value)}>
